9d2a87ff3f01157ca35ffb194f83c85ec4f9b750
chillsherry <reika.iga@gmail.com>
2021-04-12 08:48:34 +0900

    サンプルViewページ追加

---
+++ src/views/Case1.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Case1 = () => {
+  return (
+    <div>
+      <h3>事例1</h3>
+      <p>
+        事例１の紹介です。
+      </p>
+    </div>
+  )
+}
+
+export default Case1;
---
+++ src/views/Case2.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Case2 = () => {
+  return (
+    <div>
+      <h3>事例2</h3>
+      <p>
+        事例2の紹介です。
+      </p>
+    </div>
+  )
+}
+
+export default Case2;
